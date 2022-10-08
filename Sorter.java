f08586dcaa07171cabb1b829d5aa886afa65464b
AhmadAlhaid <ahmed.alhaid2002@gmail.com>
2022-10-08 21:10:22 +0300

    add selection sort

--- Sorter.java
+++ Sorter.java
@@ -1,4 +1,19 @@
 public class Sorter{
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minElementIndex] > arr[j]) {
+                    minElementIndex = j;
+                }
+            }
+            if (minElementIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minElementIndex];
+                arr[minElementIndex] = temp;
+            }
+        }
+    }
 	public static void main(String[] args){
 		
 
